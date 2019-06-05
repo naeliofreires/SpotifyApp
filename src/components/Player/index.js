a75cdac552ed2c5ddd6cf1fe50225f8434d0deae
naeliofreires <naeliofreires@gmail.com>
2019-06-05 10:55:50 -0300

    estilizando o player

---
+++ src/components/Player/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Slider from 'rc-slider';
+
+import {
+  Container, Current, Volume, Progress, Controls, Time, ProgressSlider,
+} from './styles';
+
+import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img
+        src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
+        alt="Cover Imagem"
+      />
+      <div>
+        <span>Times like these</span>
+        <small>Foo Fighters</small>
+      </div>
+    </Current>
+
+    <Progress>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="shuffle" />
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="backward" />
+        </button>
+        <button>
+          <img src={PlayIcon} alt="play" />
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="forward" />
+        </button>
+        <button>
+          <img src={RepeatIcon} alt="repeat" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:29</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ED760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+        <span>4:00</span>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#fff' }}
+        handleStyle={{ display: 'none' }}
+        value="100"
+      />
+    </Volume>
+  </Container>
+);
+export default Player;
