e6fdae013adfc666cf59836790e73ac01392539f
naeliofreires <naeliofreires@gmail.com>
2019-06-23 14:26:54 -0300

    configuracao redux e saga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
+import './config/reactotron';
 
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
@@ -9,24 +12,25 @@ import Header from './components/Header';
 import { Wrapper, Container, Content } from './styles/components';
 
 import Routes from './routes';
-
-import './config/reactotron';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-            <Routes />
-          </Content>
-        </Container>
-        <Player />
-      </Wrapper>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+        </Wrapper>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
